7f3ff46ca08d41a2d0b77347709af8e8f9596947
Ivan Petrenko <ivan.petrenko@sabre.com>
2021-02-25 19:57:54 +0100

    Formatter impl

---
+++ formatter.cpp
@@ -0,0 +1,31 @@
+#include "formatter.h"
+
+#include <iostream>
+#include <fstream>
+
+void format(const std::vector<Schedule>& schedules, int fileName)
+{
+    std::ofstream out("output.txt");
+
+    int A = schedules.size();
+    out << A << std::endl;
+
+    for (const auto& schedule : schedules)
+    {
+        const auto& i = schedule.id;
+        out << i << std::endl;
+
+        int Ei = schedule.streetAndTime.size();
+        out << Ei << std::endl;
+
+        for (const auto& streetAndTime : schedule.streetAndTime)
+        {
+            const auto& name = streetAndTime.first;
+            const auto& T = streetAndTime.second;
+
+            out << name << " " << T << std::endl;
+        }
+    }
+
+    out.close();
+}
